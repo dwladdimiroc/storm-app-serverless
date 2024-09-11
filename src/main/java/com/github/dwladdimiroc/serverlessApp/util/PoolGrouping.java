766d645ce9abbf5de7f137c1bcc787abfd63e807
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2024-09-11 16:47:06 +0200

    Refactor Pool grouping according new Storm version 2.x

--- src/main/java/com/github/dwladdimiroc/serverlessApp/util/PoolGrouping.java
+++ src/main/java/com/github/dwladdimiroc/serverlessApp/util/PoolGrouping.java
@@ -1,30 +1,22 @@
 package com.github.dwladdimiroc.serverlessApp.util;
 
-import org.apache.storm.generated.GlobalStreamId;
-import org.apache.storm.grouping.LoadAwareCustomStreamGrouping;
-import org.apache.storm.grouping.LoadMapping;
-import org.apache.storm.task.WorkerTopologyContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.storm.generated.GlobalStreamId;
+import org.apache.storm.grouping.CustomStreamGrouping;
+import org.apache.storm.task.WorkerTopologyContext;
 
-public class PoolGrouping implements LoadAwareCustomStreamGrouping {
-    private static final Logger logger = LoggerFactory.getLogger(PoolGrouping.class);
 
-    private LoadMapping load;
+public class PoolGrouping implements CustomStreamGrouping, Serializable {
+    private ArrayList<List<Integer>> choices;
+    private AtomicInteger current;
 
-    private List<Integer>[] rets;
-    private int[] targets;
-    private int[] loads;
-    private int total;
-    private long lastUpdate = 0;
-
-    private Random random;
     private Replicas replica;
-
 
     @Override
     public void prepare(WorkerTopologyContext context, GlobalStreamId stream, List<Integer> targetTasks) {
@@ -32,49 +24,29 @@ public class PoolGrouping implements LoadAwareCustomStreamGrouping {
         Thread tReplica = new Thread(replica);
         tReplica.start();
 
-        this.random = new Random();
-
-        rets = (List<Integer>[]) new List<?>[targetTasks.size()];
-        targets = new int[targetTasks.size()];
-        for (int i = 0; i < targets.length; i++) {
-            rets[i] = Arrays.asList(targetTasks.get(i));
-            targets[i] = targetTasks.get(i);
+        choices = new ArrayList<List<Integer>>(targetTasks.size());
+        for (Integer i : targetTasks) {
+            choices.add(Arrays.asList(i));
         }
-        loads = new int[targets.length];
+        current = new AtomicInteger(0);
+        Collections.shuffle(choices, new Random());
     }
 
     @Override
     public List<Integer> chooseTasks(int taskId, List<Object> values) {
-        int limit = this.replica.getNumReplicas();
-        if (targets.length < this.replica.getNumReplicas()){
-            limit = targets.length;
-        }
-
-        if ((lastUpdate + 1000) < System.currentTimeMillis()) {
-            int local_total = 0;
-            for (int i = 0; i < limit; i++) {
-                int val = (int) (101 - (load.get(targets[i]) * 100));
-                loads[i] = val;
-                local_total += val;
-            }
-            total = local_total;
-            lastUpdate = System.currentTimeMillis();
-        }
-
-        int selected = random.nextInt(total);
-        int sum = 0;
-        for (int i = 0; i < limit; i++) {
-            sum += loads[i];
-            if (selected < sum) {
-                return rets[i];
+        int rightNow;
+        int size = this.replica.getNumReplicas();
+        while (true) {
+            rightNow = current.incrementAndGet();
+            if (rightNow < size) {
+                return choices.get(rightNow);
+            } else if (rightNow == size) {
+                current.set(0);
+                return choices.get(0);
+            } else {
+                current.set(0);
+                return choices.get(new Random().nextInt(size));
             }
         }
-        return rets[0];
-    }
-
-    @Override
-    public void refreshLoad(LoadMapping loadMapping) {
-        this.load = loadMapping;
     }
 }
-
